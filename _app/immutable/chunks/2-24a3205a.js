4acb2e770dcd996a625ef6f1460503cfa47bf17c
Mustafa Akin <>
2023-01-29 14:46:25 +0300

    Updates

--- _app/immutable/chunks/2-24a3205a.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-7f7ef111.js";export{t as component};
---
+++ _app/immutable/chunks/2-848d05bb.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-3e59e6a5.js";export{t as component};
