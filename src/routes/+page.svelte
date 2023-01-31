03655acdd52f9a024504307d46ae6073845a8888
MustyDev <www.mustafakerem22@gmail.com>
2023-02-01 02:34:55 +0300

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,12 +20,11 @@
     </div>
     
     <div class="content-1" style="text-align: left; height: 100%;">
-        <h3>• Genç Yazılımcılar Kimdir?</h3>
+        <h3>Genç Yazılımcılar Kimdir?</h3>
         <p>Genç Yazılımcılar bir topluluk projesidir. Amacımız ülkemizde bulunan yazılım ve bilgisayar alanında yüksek bilgiye sahip olan gençleri bir araya getirmektir.</p>
         <br>
-        <h3>• Neden Katılmalıyım?</h3>
-        <p>Topluluğumuza ait bazı özellikler bulunmaktadır. Etkinlikler, çekilişler, yarışmalar, sosyal medya hesaplarımızdan yaptığınız projelerin yayını, çeşitli sponsorluklar ve sohbet ortamı, bu özelliklerden bazılarıdır.</p>
-        <p>Bize katılarak; tüm bu özelliklere erişim sağlıyor, Türkiye'nin ilk, gençler için gençler tarafından kurulmuş yazılım topluluğuna destek veriyorsunuz.</p>
+        <h3>Neden Katılmalıyım?</h3>
+        <p>Topluluğumuza ait bazı özellikler bulunmaktadır. Etkinlikler, çekilişler, yarışmalar, sosyal medya hesaplarımızdan yaptığınız projelerin yayını, çeşitli sponsorluklar ve sohbet ortamı, bu özelliklerden bazılarıdır. Bize katılarak; tüm bu özelliklere erişim sağlıyor, Türkiye'nin ilk, gençler için gençler tarafından kurulmuş yazılım topluluğuna destek veriyorsunuz.</p>
     </div>
     <br>
 </div>
