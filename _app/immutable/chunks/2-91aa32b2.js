64668d11abbd1dda0365668368f624ddb12eb271
Mustafa Akin <>
2023-02-10 14:28:08 +0300

    Updates

---
+++ _app/immutable/chunks/2-91aa32b2.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-4c73b6be.js";export{t as component};
--- _app/immutable/chunks/2-e16e057f.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-e37d6add.js";export{t as component};
