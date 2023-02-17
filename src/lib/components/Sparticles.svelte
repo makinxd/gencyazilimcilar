49873e2c578cf4d850419ff3cf55f08c5fc95d4c
MustyDev <www.mustafakerem22@gmail.com>
2023-02-17 14:09:50 +0300

    update

--- src/lib/components/Sparticles.svelte
+++ src/lib/components/Sparticles.svelte
@@ -3,7 +3,7 @@
     import Sparticles from "sparticles";
   
     let sparticles,
-        options = { count: 150, color: "#222222", shape: "image", speed: 5, minSize: 18, alphaSpeed: 0, drift: 1, parallax: 4, yVariance: 0, xVarience: 0, imageUrl: "images/logo/gencyazilimcilarlogo.png"};
+        options = { count: 150, color: "#303030", shape: "image", speed: 2, minSize: 18, alphaSpeed: 0, drift: 1, parallax: 2, yVariance: 0, xVarience: 0, imageUrl: "images/logo/gencyazilimcilarlogo.png"};
   
     function addSparticles(node) {
       new Sparticles(node, options, 2560, 1080);
